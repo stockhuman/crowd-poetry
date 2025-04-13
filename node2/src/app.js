508d7033bb3826c7e4555aac20151709a3469bea
Michael Hemingway <mpghemingway@gmail.com>
2025-04-12 21:18:11 -0400

    Delete node2, Teensy experiments

--- node2/src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var web_audio_api_1 = require("web-audio-api");
-var context = new web_audio_api_1.AudioContext();
-context.outStream = process.stdout;
-var o = context.createOscillator();
-o.type = "sine";
-o.connect(context.destination);
-o.start();
